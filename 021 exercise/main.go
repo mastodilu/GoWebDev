bd2eb220cd2b7090405cb3f26c89b88d5719f7cc
matteo di lucchio <matteo.dilucchio@edu.unito.it>
2019-06-30 17:22:27 +0200

    021 exercise method, route and host

--- 021 exercise/main.go
+++ 021 exercise/main.go
@@ -11,21 +11,25 @@ func handle(conn net.Conn) {
 	// read
 	scanner := bufio.NewScanner(conn)
 	lineIndex := 0
-	var host string
+	var host, route, method string
 
 	for scanner.Scan() {
-		if lineIndex == 1 {
-			// 💥 url della request
+		switch lineIndex {
+		case 0:
+			method = strings.Fields(scanner.Text())[0]
+			route = strings.Fields(scanner.Text())[1]
+		case 1:
 			host = strings.Fields(scanner.Text())[1]
 		}
 		fmt.Printf("%v\n\r", scanner.Text())
-
 		lineIndex++
 		if scanner.Text() == "" {
 			// fine header, esci dal ciclo
 			break
 		}
 	}
+	fmt.Printf("### method: %v\n", method)
+	fmt.Printf("### route: %v\n", route)
 	fmt.Printf("### host: %v\n\n", host)
 }
 
