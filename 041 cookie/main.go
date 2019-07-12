bd78cb382b6d47a0c83bbb1a4ede89f8e5362ad2
mastodilu <matteo.dilucchio@edu.unito.it>
2019-07-12 22:20:49 +0200

    write multiple cookies

--- 041 cookie/main.go
+++ 041 cookie/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
+// setMyCookie scrive un cookie chiamato 'myCookie'
 func setMyCookie(w http.ResponseWriter, r *http.Request) {
 	http.SetCookie(w, &http.Cookie{
 		Name:  "myCookie",
@@ -13,10 +14,13 @@ func setMyCookie(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Controlla i cookie.")
 }
 
+// readCookie legge tutti i cookie
 func readCookie(w http.ResponseWriter, r *http.Request) {
 	for i, c := range r.Cookies() {
 		fmt.Fprintln(w, i, *c)
 	}
+
+	fmt.Fprintln(w, "######### ######### #########")
 
 	c, err := r.Cookie("myCookie")
 	if err != nil {
@@ -25,12 +29,24 @@ func readCookie(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Found 'myCookie': ", *c)
 }
 
+// writeAll scrive un botto di cookie con un ciclo
+func writeAll(w http.ResponseWriter, r *http.Request) {
+	for i := 0; i < 5; i++ {
+		http.SetCookie(w, &http.Cookie{
+			Name:  fmt.Sprintf("Cookie-%v", i+1),
+			Value: fmt.Sprintf("Questo cookie vale %v", i+1),
+		})
+	}
+
+}
+
 func main() {
 	fmt.Println("Saluti da Linux Mint")
 	fmt.Println("Listening on port :8080")
 
 	http.HandleFunc("/", setMyCookie)
 	http.HandleFunc("/read", readCookie)
+	http.HandleFunc("/write-all", writeAll)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 
 	http.ListenAndServe(":8080", nil)
