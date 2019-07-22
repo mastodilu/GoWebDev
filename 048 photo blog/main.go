7a04e451d6c63af6baa3b6ecd64aa048b4221364
Di Lucchio Matteo <matteo.dilucchio@beta80group.it>
2019-07-22 16:55:48 +0200

    user page template; change routes handling

--- 048 photo blog/main.go
+++ 048 photo blog/main.go
@@ -7,11 +7,12 @@ import (
 	"net/http"
 )
 
+// User contiene alcuni dati utente
 type User struct {
 	Username string
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
+func homePage(w http.ResponseWriter, r *http.Request) {
 	data := struct {
 		Message string
 		Users   []User
@@ -22,7 +23,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	tpl.ExecuteTemplate(w, "homepage.gohtml", data)
 }
 
-func register(w http.ResponseWriter, r *http.Request) {
+func registerCheck(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("/register")
 
 	var email, password, username string
@@ -42,10 +43,22 @@ func register(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//TODO add user to DB
+	//TODO create session
+	//TODO create user directory
+
 	fmt.Fprintln(w, "OK")
 }
 
-func login(w http.ResponseWriter, r *http.Request) {
+func loginPage(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "login.gohtml", nil)
+}
+
+func registerPage(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "register.gohtml", nil)
+}
+
+func loginCheck(w http.ResponseWriter, r *http.Request) {
 	var email, password string
 	email = r.FormValue("email")
 	password = r.FormValue("password")
@@ -54,7 +67,15 @@ func login(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Error parsing your values", http.StatusNotFound)
 		return
 	}
+	//TODO check in DB if user exists
+	//TODO check valid session
+	//TODO load user personal page
 	fmt.Fprintln(w, "ok")
+}
+
+func userblog(w http.ResponseWriter, r *http.Request) {
+	//TODO check valid session
+	tpl.ExecuteTemplate(w, "user.gohtml", User{"Mastodilu"})
 }
 
 func init() {
@@ -69,9 +90,14 @@ func main() {
 
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 
-	http.HandleFunc("/", home)
-	http.HandleFunc("/login", login)
-	http.HandleFunc("/register", register)
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/login", loginPage)
+	http.HandleFunc("/register", registerPage)
+
+	http.HandleFunc("/user/", userblog)
+
+	http.HandleFunc("/loginCheck", loginCheck)
+	http.HandleFunc("/registerCheck", registerCheck)
 
 	http.ListenAndServe(":8080", nil)
 }
